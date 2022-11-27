1fa785907b6c634b45b3d1d89ee08e0382b866e9
kkhitesh <hkowdiki@gmail.com>
2022-11-27 23:02:57 +0530

    Improved Home Page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import { useTypewriter } from "react-simple-typewriter";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import home from "../../assets/home.png";
+import explore from "../../assets/explore.png";
 
 const Login = ({ setAddress, setIsConnected }) => {
   const { ethereum } = window;
@@ -43,9 +45,10 @@ const Login = ({ setAddress, setIsConnected }) => {
       <section className="hero h-screen overflow-hidden">
         <Navbar connectWallet={connectWallet} />
         <div className="flex flex-col items-center justify-center h-screen mx-auto">
-          <p className="font-[800] text-6xl ">Flex your NFT's</p>
+          <p className="font-[800] text-6xl ">Showcase your NFT's</p>
           <p className="font-[800] text-6xl py-5 bg-clip-text text-transparent bg-gradient-to-r from-[#3c81f6] to-[#c13584]">
             {text}
+            <Cursor cursorColor="red" />
           </p>
           <p className="text-xl ">Showcase your NFT Collection to the World</p>
           <button
@@ -56,7 +59,31 @@ const Login = ({ setAddress, setIsConnected }) => {
           </button>
         </div>
       </section>
-      <section></section>
+      <section className="flex items-center justify-between hero h-screen overflow-hidden bg-white px-10">
+        <div className="mx-20">
+          <p className="font-[800] text-6xl py-5 bg-clip-text text-transparent bg-gradient-to-r from-[#3c81f6] to-[#c13584] ml-20">
+            Showcase Your NFT's
+          </p>
+          <p className="text-xl text-[#0d192b] self-center">
+            View your Owned NFT's on the Polygon Chain
+          </p>
+        </div>
+        <img
+          src={home}
+          className="w-1/2 mx-5 rounded-lg shadow-2xl shadow-purple-300"
+        />
+      </section>
+      <section className="flex items-center justify-between hero h-screen overflow-hidden px-10">
+        <img src={explore} className="w-1/2 mx-5 rounded-lg shadow-2xl" />
+        <div className="mx-20">
+          <p className="font-[800] text-6xl py-5 bg-clip-text text-transparent bg-gradient-to-r from-[#3c81f6] to-[#c13584]">
+            Explore NFT's
+          </p>
+          <p className="text-xl self-center">
+            Search for NFT's on the Polygon Chain using Wallet Address
+          </p>
+        </div>
+      </section>
     </>
   );
 };
